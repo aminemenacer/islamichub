04332ca9202ea2ba7a95a92dd81c6da7663390a3
aminemenacer <med_amine-jsk@hotmail.com>
2024-02-21 17:50:03 +0000

    volunteer page

--- resources/js/components/VolunteerComponent.vue
+++ resources/js/components/VolunteerComponent.vue
@@ -1,18 +1,16 @@
 <template>
 <div id="app">
 
-  
-  <div class="container mt-3">
-    <div class="row h-100 align-items-center py-3">
-      <div class="container col-lg-6">
-        <h1 class="display-4">Help us grow</h1>
-        <p class="lead text-muted mb-0">We are a community-driven business dedicated to making a positive impact through volunteerism. Our organization believes in the power of collective effort to bring about meaningful change. Whether you are a student, professional, retiree, or someone with a passion for giving back, Helping Hands Haven welcomes individuals from all walks of life.</p>
-        <button type="button btn-xl" class="btn btn-outline-dark mt-3">Learn more</button>
-
-      </div>
-      <div class="col-lg-5 mt-5 d-none d-lg-block"><img src="https://bootstrapious.com/i/snippets/sn-about/illus.png" alt="" class="img-fluid"></div>
-    </div>
+ <div class="container pt-4">
+  <h1 class="display-4 text-center">Help us grow</h1>
+  <div class="row h-100 align-items-center py-3">
+   <div class="container-fluid col-lg-6">
+    <p class="lead text-muted mb-0">We are a community-driven business dedicated to making a positive impact through volunteerism. Our organization believes in the power of collective effort to bring about meaningful change. Whether you are a student, professional, retiree, or someone with a passion for giving back, Helping Hands Haven welcomes individuals from all walks of life.</p>
+    <button type="button btn-xl" class="btn-md btn btn-outline-success mt-3">Learn more</button>
+   </div>
+   <div class="col-lg-6 d-none d-lg-block"><img src="/images/helpp.png" alt="" class="img-fluid"></div>
   </div>
+ </div>
 </div>
 </template>
 
