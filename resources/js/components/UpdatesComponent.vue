91cc0afb6a8ec9d968393759ab6ad125049d3784
aminemenacer <med_amine-jsk@hotmail.com>
2024-06-04 23:10:06 +0100

    updated the updates page

--- resources/js/components/UpdatesComponent.vue
+++ resources/js/components/UpdatesComponent.vue
@@ -1,24 +1,45 @@
 <template>
 <div class="container">
 
- <div class="row ml-4 mt-5 container">
- 
-  <h3 class="text-left mt-3 container" style="line-height: 1.7em; font-family:inter; "><img src="/images/art.png" style="width: 40px;" class="mb-1 mr-2" /> Welcome to the Islamic Connect Beta Version.</h3>
-  <div class="col-md-6"><h5 class="text-left text-secondary" width="80%" style="line-height: 1.7em; font-family:inter">MAY 27th, 2024</h5>
-  <p class="text-left text-secondary " style="line-height: 1.7em; font-family:inter">"Islamic Connect Beta Version provides an exciting preview of our inclusive online Islamic community. Engage with our evolving platform, designed to foster meaningful connections and enhance your spiritual journey. We invite you to join us in shaping the future of Islamic Connect through your valuable feedback and active participation". We offer many features such as:</p>
+ <div class="row ml-4 mt-3 container-fluid">
 
-  <div>
-   <li style="padding:5px"><strong>Translation, Tafseer and Transliteration</strong></li>
-   <li style="padding:5px"><strong>Searchable Quran Translations</strong></li>
-   <li style="padding:5px"><strong>Notable Annotation</strong></li>
-   <li style="padding:5px"><strong>Verse screen-shoting</strong></li>
-   <li style="padding:5px"><strong>Mobile friendly design</strong></li>
+  <h3 class="text-left mt-3 mb-2 container" style="line-height: 1.7em; font-family:inter; "><img src="/images/art.png" style="width: 40px;" class="mb-1 mr-2" /> Unlock Your Quranic Journey with Ayah Controls</h3>
+  <div class="col-md-6">
+   <h5 class="text-left text-secondary" width="80%" style="line-height: 1.7em; font-family:inter">June 4th, 2024</h5>
+   <p class="text-left text-secondary " style="line-height: 1.7em; font-family:inter">"Having Ayah controls as a feature in an application, particularly one focused on Quranic study or recitation, offers several benefits:</p>
+
+   <div>
+    <li style="padding:5px"><strong>Enhanced User Experience:</strong> Ayah controls allow users to navigate through different verses of the Quran with ease. Users can quickly switch between Ayahs without needing to manually scroll through the text.</li>
+    <li style="padding:5px"><strong>Convenience in Memorization:</strong> For those engaged in memorizing Quranic verses (Hifz), Ayah controls provide a convenient way to repeat and review specific verses or sections.</li>
+    <li style="padding:5px"><strong>Accessibility:</strong> Ayah controls can improve accessibility for users with disabilities or those who may find it challenging to navigate large amounts of text, making the Quran more accessible to a wider audience.</li>    
+   </div>
   </div>
-  <p class="text-left text-secondary mt-1" style="line-height: 1.7em; font-family:inter">and much, much more.</p>
+  <div class="col-md-6 ">
+   <img src="images/ayah_controls.png" style="width:70%" />
+  </div>
  </div>
- <div class="col-md-6 ">
-  <img src="images/main-design.png" style="width:100%"/>
- </div>
+
+ <hr>
+
+ <div class="row ml-4 mt-3 container">
+
+  <h3 class="text-left mt-3 container" style="line-height: 1.7em; font-family:inter; "><img src="/images/art.png" style="width: 40px;" class="mb-1 mr-2" /> Welcome to the Islamic Connect Beta Version</h3>
+  <div class="col-md-6">
+   <h5 class="text-left text-secondary" width="80%" style="line-height: 1.7em; font-family:inter">May 27th, 2024</h5>
+   <p class="text-left text-secondary " style="line-height: 1.7em; font-family:inter">"Islamic Connect Beta Version provides an exciting preview of our inclusive online Islamic community. Engage with our evolving platform, designed to foster meaningful connections and enhance your spiritual journey. We invite you to join us in shaping the future of Islamic Connect through your valuable feedback and active participation". We offer many features such as:</p>
+
+   <div>
+    <li style="padding:5px"><strong>Translation, Tafseer and Transliteration</strong></li>
+    <li style="padding:5px"><strong>Searchable Quran Translations</strong></li>
+    <li style="padding:5px"><strong>Notable Annotation</strong></li>
+    <li style="padding:5px"><strong>Verse screen-shoting</strong></li>
+    <li style="padding:5px"><strong>Mobile friendly design</strong></li>
+   </div>
+   <p class="text-left text-secondary mt-1" style="line-height: 1.7em; font-family:inter">and much, much more.</p>
+  </div>
+  <div class="col-md-6 ">
+   <img src="images/main-design.png" style="width:70%" />
+  </div>
  </div>
 
 </div>
