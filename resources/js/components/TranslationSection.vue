bab0c64c0e6234c7c679de9ee860700318b3f745
aminemenacer <med_amine-jsk@hotmail.com>
2024-10-06 18:23:27 +0100

    upgraded tts feature

--- resources/js/components/TranslationSection.vue
+++ resources/js/components/TranslationSection.vue
@@ -1,73 +1,72 @@
 <template>
 <div class="w-100 my-element" :class="{'full-screen': isFullScreen}">
  <button v-if="isFullScreen" @click="toggleFullScreen" class="close-button mb-3 text-left btn btn-secondary">Close</button>
-<div ref="targetTranslationElement">
-    <AyahInfo :information="information" />
-    <div
-      @touchstart="handleTouchStart"
-      @touchmove="handleTouchMove"
-      @touchend="handleTouchEnd"
-      class="swipeable-div w-100"
-    >
-      <MainAyah :information="information" />
-      <div ref="heading3" class="row text-left">
-        <h4
-          :style="{ fontSize: currentFontSize + 'px' }"
-          class="text-left ayah-translation col-md-11"
-          style="line-height: 1.6em"
-        >
-          {{ expanded ? information.translation : truncatedText(information.translation) }}
-          <template v-if="showMoreLink && information.translation.length > 100">
-            <a href="#" @click.prevent="toggleExpand">{{ expanded ? 'Show Less' : 'Show More' }}</a>
-          </template>
-        </h4>
-        <div
-          class="col-md-1 mt-4"
-          style="cursor: pointer; display: flex; flex-direction: column; align-items: center; justify-content: space-between; height: 150px;"
-        >
-        <CopyTranslationText :textToCopy="combinedText" />
-          <i
-            @click="toggleSpeech"
-            style="cursor: pointer;"
-            aria-placeholder="Play translation audio"
-            :class="isReading ? 'bi-pause-circle-fill' : 'bi-play-circle-fill'"
-            class="bi ml-2 mr-2 h3 custom-icon-play"
-          ></i>
-          <i class="bi bi-plus-circle-fill h3 custom-icon-increase" aria-placeholder="Increase text size" @click="increaseFontSize"></i>
-          <i class="bi bi-dash-circle-fill h3 custom-icon-decrease" aria-placeholder="Decrease text size" @click="decreaseFontSize"></i>
-        </div>
-        <Translator translator="Ahmed Ali" />
+ <div ref="targetTranslationElement">
+  <AyahInfo :information="information" />
+  <div @touchstart="handleTouchStart" @touchmove="handleTouchMove" @touchend="handleTouchEnd" class="swipeable-div w-100">
+   <MainAyah :information="information" />
+   <div ref="heading3" class="row text-left">
+    <h4 :style="{ fontSize: currentFontSize + 'px' }" class="text-left ayah-translation col-md-11" style="line-height: 1.6em">
+     {{ expanded ? information.translation : truncatedText(information.translation) }}
+     <template v-if="showMoreLink && information.translation.length > 100">
+      <a href="#" @click.prevent="toggleExpand">{{ expanded ? 'Show Less' : 'Show More' }}</a>
+     </template>
+    </h4>
+    <div class="col-md-1 mt-4" style="cursor: pointer; display: flex; flex-direction: column; align-items: center; justify-content: space-between; height: 150px;">
+     <CopyTranslationText :textToCopy="combinedText" />
+     <i @click="toggleSpeech" style="cursor: pointer;" aria-placeholder="Play translation audio" :class="isReading ? 'bi-pause-circle-fill' : 'bi-play-circle-fill'" class="bi ml-2 mr-2 h3 custom-icon-play"></i>
+     <i class="bi bi-plus-circle-fill h3 custom-icon-increase" aria-placeholder="Increase text size" @click="increaseFontSize"></i>
+     <i class="bi bi-dash-circle-fill h3 custom-icon-decrease" aria-placeholder="Decrease text size" @click="decreaseFontSize"></i>
+    </div>
+
+    <Translator translator="Ahmed Ali" />
+
+    <div class="container text-center pt-2">
+     <div class="row">
+      <div class="col-md-6">
+       <select class="form-control container" v-model="selectedVoice" @change="changeVoice($event.target.value)">
+        <option v-for="voice in voices" :key="voice.name" :value="voice">
+         {{ voice.name }} ({{ voice.lang }})
+        </option>
+       </select>
       </div>
-      <AlertModal
-        :showAlertText="showAlertText"
-        :showAlert="showAlert"
-        :showErrorAlert="showErrorAlert"
-        :showAlertTextNote="showAlertTextNote"
-        @close-alert-text="closeAlertText"
-      />
-    
- </div>
- <!-- WhatsApp Share Button -->
- <div class="row mobile-only pt-3" style="color:white">
-  <div class="col-6 ">
+      <div class="col-md-3 pt-2">
+       <label>
+        Rate: <input type="range" min="0.5" max="2" step="0.1" v-model="rate" @input="adjustRate($event.target.value)">
+       </label>
+      </div>
+      <div class="col-md-3 pt-2">
+       <label>
+        Pitch: <input type="range" min="0.5" max="2" step="0.1" v-model="pitch" @input="adjustPitch($event.target.value)">
+       </label>
+      </div>
+     </div>
+    </div>
+
+   </div>
+   <AlertModal :showAlertText="showAlertText" :showAlert="showAlert" :showErrorAlert="showErrorAlert" :showAlertTextNote="showAlertTextNote" @close-alert-text="closeAlertText" />
+
+  </div>
+  <!-- WhatsApp Share Button -->
+  <div class="row mobile-only pt-3" style="color:white">
+   <div class="col-6 ">
     <!-- Add right padding to create space between the buttons -->
     <div @click="shareOnWhatsApp" style="cursor: pointer;">
-      <i class="bi bi-whatsapp  h3" style="color:black"></i>
-      <p style="font-size: 13px; margin: 0;color:black" class="pt-2">Share via WhatsApp</p>
+     <i class="bi bi-whatsapp  h3" style="color:black"></i>
+     <p style="font-size: 13px; margin: 0;color:black" class="pt-2">Share via WhatsApp</p>
     </div>
-  </div>
+   </div>
 
-  <div class="col-6 pr-2">
+   <div class="col-6 pr-2">
     <!-- Add right padding to create space between the buttons -->
     <div @click="shareOnTwitter" style="cursor: pointer;">
-      <i class="bi bi-twitter-x  h3" style="color:black"></i>
-      <p style="font-size: 13px; margin: 0;color:black" class="pt-2">Share via X</p>
+     <i class="bi bi-twitter-x  h3" style="color:black"></i>
+     <p style="font-size: 13px; margin: 0;color:black" class="pt-2">Share via X</p>
     </div>
+   </div>
   </div>
  </div>
 </div>
-</div>
-
 </template>
 
 <script>
@@ -78,12 +77,7 @@ import AlertModal from './modals/AlertModal.vue';
 import ScreenReader from './accesibility/ScreenReader.vue';
 
 export default {
-  mounted() {
-    const savedFontSize = localStorage.getItem('fontSize');
-    if (savedFontSize) {
-      this.currentFontSize = parseInt(savedFontSize, 10); // Set the font size from localStorage
-    }
-  },
+
  name: 'TranslationSection',
  components: {
   AyahInfo,
@@ -131,34 +125,55 @@ export default {
    default: false
   }
  },
- computed:{
+ computed: {
   combinedText() {
-    // Get the ayah_text from information
-    const ayahText = typeof this.information.ayah_text === 'object'
-      ? this.information.ayah_text.text
-      : this.information.ayah_text;
+   // Get the ayah_text from information
+   const ayahText = typeof this.information.ayah_text === 'object' ?
+    this.information.ayah_text.text :
+    this.information.ayah_text;
 
-    // Return the formatted string
-    return `Translation: ${this.ayah_text}`;
+   // Return the formatted string
+   return `Translation: ${this.ayah_text}`;
   }
  },
  data() {
   return {
-    resetDisabled: true,
-    utterance: null,
-    currentFontSize: 20,
+   isReading: false,
+   resetDisabled: true,
+   utterance: null,
+   currentFontSize: 20,
+   selectedVoiceName: '',
+   rate: 1,
+   pitch: 1,
+   voices: []
+  }
+ },
+ mounted() {
+  const savedFontSize = localStorage.getItem('fontSize');
+  if (savedFontSize) {
+   this.currentFontSize = parseInt(savedFontSize, 10); // Set the font size from localStorage
+  }
+  this.loadVoices();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+   speechSynthesis.onvoiceschanged = this.loadVoices;
   }
  },
  methods: {
+  loadVoices() {
+   this.voices = speechSynthesis.getVoices();
+   if (this.voices.length > 0) {
+    this.selectBestVoice();
+   }
+  },
   increaseFontSize() {
-    this.currentFontSize += 2; // Increase font size by 2px
-    this.saveFontSize();
+   this.currentFontSize += 2; // Increase font size by 2px
+   this.saveFontSize();
   },
   decreaseFontSize() {
-    if (this.currentFontSize > 10) {
-      this.currentFontSize -= 2; // Decrease font size by 2px, limit to 10px minimum
-      this.saveFontSize();
-    }
+   if (this.currentFontSize > 10) {
+    this.currentFontSize -= 2; // Decrease font size by 2px, limit to 10px minimum
+    this.saveFontSize();
+   }
   },
   // resetFontSize() {
   //   this.currentFontSize = 20; // Reset to default font size
@@ -166,35 +181,83 @@ export default {
   //   this.resetDisabled = true; // Disable the reset button after resetting the font size
   // },
   saveFontSize() {
-    // Save the current font size to localStorage
-    localStorage.setItem('fontSize', this.currentFontSize);
+   // Save the current font size to localStorage
+   localStorage.setItem('fontSize', this.currentFontSize);
+  },
+  selectBestVoice() {
+   const preferredVoices = [
+    'Google UK English Female',
+    'Microsoft Zira Desktop - English (United States)',
+    'Samantha',
+    'Google US English',
+    'Microsoft David Desktop - English (United States)',
+   ];
+
+   for (const preferredVoice of preferredVoices) {
+    const voice = this.voices.find(v => v.name === preferredVoice);
+    if (voice) {
+     this.selectedVoice = voice;
+     return;
+    }
+   }
+
+   if (this.voices.length > 0) {
+    this.selectedVoiceName = this.voices[0].name;
+   }
+
+   // If no preferred voice is found, choose the first available voice
+   this.selectedVoice = this.voices[0];
+  },
+  changeVoice(voiceName) {
+   this.selectedVoiceName = voiceName;
+  },
+  adjustRate(value) {
+   this.rate = parseFloat(value);
+  },
+  adjustPitch(value) {
+   this.pitch = parseFloat(value);
+  },
+
+  stopSpeech() {
+   if (this.isReading) {
+    window.speechSynthesis.cancel();
+    this.isReading = false;
+   }
   },
   toggleSpeech() {
-    if (this.isReading) {
-      this.stopReading();
-    } else {
-      this.readTextAloud();
-    }
+   if (this.isReading) {
+    this.stopSpeech();
+   } else {
+    this.readTextAloud();
+   }
   },
   // Read the displayed text aloud
   readTextAloud() {
-    const text = this.expanded ? this.information.translation : this.truncatedText(this.information.translation);
-    this.utterance = new SpeechSynthesisUtterance(text);
+   const text = this.expanded ? this.information.translation : this.truncatedText(this.information.translation);
+   this.utterance = new SpeechSynthesisUtterance(text);
 
-    
-    // Handle end of speech event
-    this.utterance.onend = () => {
-      this.isReading = false;
-    };
+   const selectedVoice = this.voices.find(voice => voice.name === this.selectedVoiceName);
+   if (this.selectedVoice) {
+    this.utterance.voice = this.selectedVoice;
+   }
 
-    // Start speaking
-    this.isReading = true;
-    window.speechSynthesis.speak(this.utterance);
+   this.utterance.rate = this.rate;
+   this.utterance.pitch = this.pitch;
+
+   // Handle end of speech event
+   this.utterance.onend = () => {
+    this.isReading = false;
+   };
+
+   // Start speaking
+   this.isReading = true;
+   window.speechSynthesis.speak(this.utterance);
   },
-  // Stop reading
-  stopReading() {
+  stopSpeech() {
+   if (this.isReading) {
     window.speechSynthesis.cancel();
     this.isReading = false;
+   }
   },
   setAyahText(text) {
    this.ayahText = text; // Capture the ayah text from the child component
@@ -227,7 +290,7 @@ export default {
    const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}`;
    window.open(url, '_blank');
   },
-  
+
   toggleFullScreen() {
    this.$emit('toggle-full-screen');
   },
@@ -250,27 +313,36 @@ export default {
    this.$emit('close-alert-text');
   }
  },
- 
+
 };
 </script>
 
 <style scoped>
+.custom-icon-play:hover {
+ color: rgb(13, 182, 145);
+ /* Default color */
+ transition: color 0.3s ease;
+ /* Smooth transition */
+}
 
-.custom-icon-play:hover{
-  color: rgb(13, 182, 145); /* Default color */
-  transition: color 0.3s ease; /* Smooth transition */
+.custom-icon-increase:hover {
+ color: rgb(13, 182, 145);
+ /* Default color */
+ transition: color 0.3s ease;
+ /* Smooth transition */
 }
-.custom-icon-increase:hover{
-  color: rgb(13, 182, 145); /* Default color */
-  transition: color 0.3s ease; /* Smooth transition */
+
+.custom-icon-decrease:hover {
+ color: rgb(13, 182, 145);
+ /* Default color */
+ transition: color 0.3s ease;
+ /* Smooth transition */
 }
-.custom-icon-decrease:hover{
-  color: rgb(13, 182, 145); /* Default color */
-  transition: color 0.3s ease; /* Smooth transition */
-}
+
 .text-muted {
-  color: lightgrey;
+ color: lightgrey;
 }
+
 .full-screen {
  position: fixed;
  top: 0;
@@ -289,7 +361,6 @@ export default {
 .ayah-translation {
  font-size: 1.2rem;
 }
-
 
 .btn {
  display: flex;
@@ -321,5 +392,4 @@ export default {
   display: none;
  }
 }
-
 </style>
