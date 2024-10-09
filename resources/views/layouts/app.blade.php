22f86849afe8e3f352e8dd6eb27f8ac7e19ec303
aminemenacer <med_amine-jsk@hotmail.com>
2024-10-09 01:28:47 +0100

    updated nav links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,20 +59,23 @@
                             <h6><a style="color: black;" class="nav-link ml-3 pl-3" href="/about"><b>About Us</b></a></h6>
                         </li>
                         <li class="nav-item mt-3">
+                            <h6><a style="color: black;" class="nav-link ml-3 pl-3" href="/join_us"><b>Our Mission</b></a></h6>
+                        </li>
+                        <li class="nav-item mt-3">
                             <h6><a style="color: black;" class="nav-link ml-3 pl-3" href="/faq"><b>FAQ's</b></a></h6>
                         </li>
-                        {{-- <li class="nav-item mt-3">
-                            <h6><a style="color: black;" class="nav-link ml-3 pl-3" href="/join_us"><b>Mailing List</b></a></h6>
-                        </li> --}}
                         <li class="nav-item mt-3">
-                            <h6><a style="color: black;" class="nav-link ml-3 pl-3" href="/pricing"><b>Pricings</b></a></h6>
+                            <h6><a style="color: black;" class="nav-link ml-3 pl-3" href="/join_us"><b>Mail List</b></a></h6>
                         </li>
+                        {{-- <li class="nav-item mt-3">
+                            <h6><a style="color: black;" class="nav-link ml-3 pl-3" href="/pricing"><b>Pricings</b></a></h6>
+                        </li> --}}
                         <li class="nav-item mt-3">
                             <h6><a style="color: black;" class="nav-link ml-3 pl-3" href="/updates"><b>Updates</b></a></h6>
                         </li>
-                        <li class="nav-item mt-3">
+                        {{-- <li class="nav-item mt-3">
                             <h6><a style="color: black;" class="nav-link ml-3 pl-3" href="/group_notes"><b>The Board</b></a></h6>
-                        </li>
+                        </li> --}}
                         <li class="nav-item mt-3">
                             <h6><a style="color: black;" class="nav-link ml-3 pl-3" href="/support"><b>Donation</b></a></h6>
                         </li>
