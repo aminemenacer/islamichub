1871ea8837ac54509fc0f4f996f0e9212352bd67
aminemenacer <med_amine-jsk@hotmail.com>
2024-11-28 15:27:26 +0000

    pricing plan table

--- resources/js/components/PricingComponent.vue
+++ resources/js/components/PricingComponent.vue
@@ -1,6 +1,6 @@
 <template>
 <div id="app" class="container-fluid pt-3">
-  <section class="py-3">
+  <!-- <section class="py-3">
     <div class="container">
       <div class="row justify-content-center text-center mb-2 mb-lg-4">
         <div class="col-12 col-lg-8 col-xxl-7 text-center mx-auto">
@@ -32,6 +32,83 @@
               <button class="btn btn-lg" style="background:rgba(0, 191, 166);color:white" @click="initiateCheckout(key)">
                 Get Started
               </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section> -->
+
+  <section class="py-3">
+    <div class="price-sec-wrap">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-lg-8 col-xxl-7 text-center mx-auto">
+            <h2 class="display-4 fw-bold">Our Pricings</h2>
+            <p class="lead">We offer flexible pricing plans tailored to meet your goals. Whether you're just starting out or need advanced features, select the plan that works best for you and get started today.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="price-box">
+              <div class="">
+                <div class="price-label basic">Basic Plan</div>
+                <div class="price">£ 0</div>
+                <div class="price-info">Per Month</div>
+              </div>
+              <div class="info">
+                <ul>
+                  <li><img src="images/check.png" width="20px" class="mr-2"/>Voice search with instant quranic results.</li>
+                  <li><img src="images/check.png" width="20px" class="mr-2"/>Speech to text audio recording for user feedback.</li>
+                  <li><img src="images/check.png" width="20px" class="mr-2"/>Toggle between basic and advanced mode.</li>
+                  <li><img src="images/check.png" width="20px" class="mr-2"/>Multiple translations and audio recitations (In progress).</li>
+                  <li><img src="images/check.png" width="20px" class="mr-2"/>Generate text summary (10 daily).</li>
+                  <li><img src="images/check.png" width="20px" class="mr-2"/>Unlimited bookmark and basic notes storage.</li>
+                  <li><img src="images/check.png" width="20px" class="mr-2"/>Basic Surah information.</li>
+                </ul>
+                <!-- <a href="#" class="plan-btn">Join Basic Plan</a> -->
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="price-box">
+              <div class="">
+                <div class="price-label value">Value Plan</div>
+                <div class="price">£ 1.99</div>
+                <div class="price-info">Per Month</div>
+              </div>
+              <div class="info">
+                <ul>
+                  <li><img src="images/check.png" width="20px" class="mr-2"/>Audio sync with Word line Highlighting </li>
+                  <li><img src="images/check.png" width="20px" class="mr-2"/>Search for blogs, articles, podcasts etc</li>
+                  <li><img src="images/check.png" width="20px" class="mr-2"/>Generate text summary (20 daily).</li>
+                  <li><img src="images/check.png" width="20px" class="mr-2"/>Advanced and customized Editor.</li>
+                  <li><img src="images/check.png" width="20px" class="mr-2"/>Speech to text for english translations and tafseer's.</li>
+                  <li><img src="images/check.png" width="20px" class="mr-2"/>Text to speech for note recordings (10 daily).</li>
+                  <li><img src="images/check.png" width="20px" class="mr-2"/>Download screen capture (10 daily).</li>
+                  <li><img src="images/check.png" width="20px" class="mr-2"/>Export Quranic verses via CSV, Word or PDF.</li>
+                </ul>
+                <a href="#" class="plan-btn">Join Value Plan</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="price-box">
+              <div class="">
+                <div class="price-label premium">Premium Plan</div>
+                <div class="price">£ 2.99</div>
+                <div class="price-info">Per Month</div>
+              </div>
+              <div class="info">
+                <ul>
+                  <li><img src="images/check.png" width="20px" class="mr-2"/>Download blogs, articles for offline reading.</li>
+                  <li><img src="images/check.png" width="20px" class="mr-2"/>Generate unlimited text summary with PDF exports.</li>
+                  <li><img src="images/check.png" width="20px" class="mr-2"/>Customize speech to text for better audio experience.</li>
+                  <li><img src="images/check.png" width="20px" class="mr-2"/>Text to speech for note recordings (unlimited).</li>
+                  <li><img src="images/check.png" width="20px" class="mr-2"/>Download screen capture (unlimited).</li>
+                </ul>
+                <a href="#" class="plan-btn">Join Premium Plan</a>
+              </div>
             </div>
           </div>
         </div>
@@ -113,8 +190,149 @@ export default {
 </script>
 
 <style scoped>
+.price-sec-wrap {
+  width: 100%;
+  float: left;
+  font-family: 'Lato', sans-serif;
+}
 
-.card{
+.main-heading {
+  text-align: center;
+  font-weight: 600;
+  padding-bottom: 15px;
+  position: relative;
+  text-transform: capitalize;
+  font-size: 24px;
+  margin-bottom: 25px;
+}
+
+.price-box {
+  box-shadow: 0 0 35px rgba(0, 0, 0, 0.10);
+  padding: 20px;
+  background: #fff;
+  border-radius: 4px;
+}
+
+.price-box ul {
+  padding: 10px 0px 30px;
+  margin: 17px 0 0 0;
+  list-style: none;
+  border-top: solid 1px #e9e9e9;
+}
+
+.price-box ul li {
+  padding: 7px 0;
+  font-size: 14px;
+  color: #808080;
+}
+
+.price-box ul li .fas {
+  color: #68AE4A;
+  margin-right: 7px;
+  font-size: 12px;
+}
+
+.price-label {
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 1.34;
+  margin-bottom: 0;
+  padding: 6px 15px;
+  display: inline-block;
+  border-radius: 3px;
+}
+
+.price-label.basic {
+  background: #E8EAF6;
+  color: #3F51B5;
+}
+
+.price-label.value {
+  background: #E8F5E9;
+  color: #4CAF50;
+}
+
+.price-label.premium {
+  background: #FBE9E7;
+  color: #FF5722;
+}
+
+.price {
+  font-size: 44px;
+  line-height: 44px;
+  margin: 15px 0 6px;
+  font-weight: 900;
+}
+
+.price-info {
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.67;
+  color: inherit;
+  width: 100%;
+  margin: 0;
+  color: #989898;
+}
+
+.plan-btn {
+  text-transform: uppercase;
+  font-weight: 600;
+  display: block;
+  padding: 11px 30px;
+  border: 2px solid #b3b3b3;
+  color: #000;
+  margin-top: 5px;
+  overflow: hidden;
+  position: relative;
+  z-index: 1;
+  margin: 0;
+  border-radius: 5px;
+  text-decoration: none;
+  width: 100%;
+  text-align: center;
+  font-size: 14px;
+}
+
+.plan-btn::after {
+  position: absolute;
+  left: -100%;
+  top: 0;
+  content: "";
+  height: 100%;
+  width: 100%;
+  background: #000;
+  z-index: -1;
+  transition: all 0.35s ease-in-out;
+}
+
+.plan-btn:hover::after {
+  left: 0;
+}
+
+.plan-btn:hover,
+.plan-btn:focus {
+  text-decoration: none;
+  color: #fff;
+  border: 2px solid #000;
+}
+
+@media (max-width: 991px) {
+  .price-box {
+    margin-bottom: 20px;
+  }
+}
+
+@media (max-width: 575px) {
+  .main-heading {
+    font-size: 21px;
+  }
+
+  .price-box {
+    margin-bottom: 20px;
+  }
+}
+
+/* .card{
   box-shadow: 24px 24px 80px rgba(0,0,0,0.1);
   box-sizing: border-box;
   border-radius: 12px;
@@ -129,5 +347,5 @@ export default {
   margin: 10px;
   padding: 10px;
   cursor: pointer;
-}
+} */
 </style>
